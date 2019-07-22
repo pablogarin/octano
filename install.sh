ebb41c736e9060bfe58e329ce4b84ef42e6c8277
pablogarin <pablo.garin@hotmail.com>
2019-07-22 16:20:43 -0400

    Se agrega flag -y -q=2 al instalador

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo "deb https://download.virtualbox.org/virtualbox/debian $(cat /etc/os-release|grep VERSION=|cut -d'=' -f2|cut -d'(' -f2|cut -d')' -f1) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get -y -qq update
-sudo apt-get -y -qq upgrade
-sudo apt-get install vagrant virtualbox-6.0
+sudo apt-get -y -q=2 update
+sudo apt-get -y -q=2 upgrade
+sudo apt-get -y -q=2 install vagrant virtualbox-6.0
